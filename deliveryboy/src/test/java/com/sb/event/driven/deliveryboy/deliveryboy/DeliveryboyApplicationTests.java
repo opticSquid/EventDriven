8c8373549cccd9c5d17a77dd366255f710faee8b
Soumalya Bhattacharya <soumalyabhattacharya6@gmail.com>
2023-08-30 19:48:00 +0530

    Created 2 services and conigured kafka with docker-compose

---
+++ deliveryboy/src/test/java/com/sb/event/driven/deliveryboy/deliveryboy/DeliveryboyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sb.event.driven.deliveryboy.deliveryboy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryboyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
